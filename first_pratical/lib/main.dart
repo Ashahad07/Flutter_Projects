6448bd99e490a199ab9a129bd0f6f70b6a3aeb69
Ashahad07 <ashahadshaikh0007@gmail.com>
2025-01-13 19:14:57 +0530

    First Flutter Project

---
+++ first_pratical/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 32, 32, 32),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10))),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      margin: EdgeInsets.only(top: 16, left: 20, right: 30),
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 24, 36, 46),
+                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                    ),
+                    Text(
+                      "John Doe",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
